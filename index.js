94ded59ba07378ebce51b988da6f83e1d7ce6f55
yzouhar <yzouhar377@gmail.com>
2024-11-23 21:37:42 -0500

    chnaged flex layout and added button

---
+++ index.js
@@ -0,0 +1,8 @@
+let userUploadedImage = document.getElementById("userUpload").value;
+let uploadButton = document.getElementById('uploadButton');
+
+uploadButton.addEventListener("click", () => {
+    console.log(userUploadedImage);
+    console.log("hi")
+
+});
